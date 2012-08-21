fbf4a006886348796978df980e0e6188b9674cd8
graywolf <graywolfs@gmail.com>
2012-08-21 13:47:22 +0300

    added synatx highligh to blog entries, various improvements on the front page

--- views.py
+++ views.py
@@ -1,12 +1,18 @@
 from misc.decorators import render_to, BaseHandler 
 from skollfolio.models import PortfolioProject
+from skollblog.models import BlogPost
 
 class home(BaseHandler):
     @render_to("index.html", 0)
     def get(self):
-        projects = PortfolioProject.all().fetch(None)
+        featured = PortfolioProject.all().filter("is_featured =", True).fetch(None)
+        projects = PortfolioProject.all().order("-date_created").fetch(4)
+        posts = BlogPost.all().order("-date_created").fetch(2)
+
         return {
+            "featured": featured,
             "projects": projects,
+            "posts": posts,
         }
 
 
